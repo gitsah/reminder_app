bc2ef871b4d9db7f68a1ef11c812446375ada622
Sahand-Admin <ghotbijr@gmail.com>
2018-06-06 17:34:13 -0700

    Began setting up listview to display reminders

--- app/src/main/java/com/example/sahand/reminder_app/ReminderFragment.java
+++ app/src/main/java/com/example/sahand/reminder_app/ReminderFragment.java
@@ -1,27 +1,70 @@
 package com.example.sahand.reminder_app;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReminderFragment extends Fragment {
 
-    public ReminderFragment() {
-    }
+    private ListView mListView;
+
+    private List<Reminder> reminders;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_reminder_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_reminder, container, false);
+
+        mListView = view.findViewById(R.id.reminder_list_view);
 
         return view;
+    }
+
+    class ReminderAdapter extends BaseAdapter {
+        private Context mContext;
+        private LayoutInflater mInflater;
+        private ArrayList<Reminder> mDataSource;
+
+        public ReminderAdapter(Context context, ArrayList<Reminder> reminders) {
+            mContext = context;
+            mDataSource = reminders;
+            mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return mDataSource.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mDataSource.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // Get view for row item
+            if (convertView == null) convertView = mInflater.inflate(R.layout.list_item_reminder, parent, false);
+
+            return convertView;
+        }
     }
 }
