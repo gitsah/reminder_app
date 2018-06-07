439371c35c444adadc02f3b3746b4be1229ac2d0
ryanybrennan <ryanybrennan@msn.com>
2018-06-06 19:58:35 -0700

    created activity layout page, added strings

--- app/src/main/java/com/example/sahand/reminder_app/Reminder.java
+++ app/src/main/java/com/example/sahand/reminder_app/Reminder.java
@@ -3,6 +3,7 @@ package com.example.sahand.reminder_app;
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
 
 @Entity
 public class Reminder {
