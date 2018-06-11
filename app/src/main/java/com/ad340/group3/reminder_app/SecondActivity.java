3d1fee6fb9dc3f0c64b6811fc2430bc4f90deec8
ryanybrennan <ryanybrennan@msn.com>
2018-06-10 22:02:15 -0700

    added alarm functionality in second activity and an alarmreceiver class

--- app/src/main/java/com/ad340/group3/reminder_app/SecondActivity.java
+++ app/src/main/java/com/ad340/group3/reminder_app/SecondActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.lang.ref.WeakReference;
+import java.util.Date;
 import java.util.Locale;
 
 public class SecondActivity extends Activity {
@@ -21,6 +22,7 @@ public class SecondActivity extends Activity {
     private EditText messageText;
     private EditText snoozeInterval;
     private boolean snoozeStatus;
+    private Date currentDate;
 
     Calendar myCalendar = Calendar.getInstance();
 
@@ -90,6 +92,8 @@ public class SecondActivity extends Activity {
             ReminderDatabase db = ReminderDatabaseSingleton.getDatabase(secondActivity);
 
             db.reminderDao().insert(reminder);
+            AlarmReceiver.setAlarm(secondActivity, reminder);
+
             return reminder;
         }
 
@@ -108,6 +112,61 @@ public class SecondActivity extends Activity {
         String myFormat = "MM/dd/yyyy";
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
         dateText.setText(sdf.format(myCalendar.getTime()));
+    }
+
+//    public int getTimeSpan(Reminder reminder){
+//        String myFormat = "MM/dd/yyyy";
+//        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+//
+//        String timeFormat = "HH:mm";
+//        SimpleDateFormat stf = new SimpleDateFormat(timeFormat);
+//
+//        currentDate = new Date();
+//        String currentDate1 = sdf.format(currentDate);
+//        String currentTime1 = stf.format(currentDate);
+//
+//        String[] date1 = currentDate1.split("/");
+//        String[] time1 = currentTime1.split(":");
+//        String[] date2 = reminder.getDate().split("/");
+//        String[] time2 = reminder.getTime().split(":");
+//
+//        int tMonth = Integer.parseInt(date1[0]);
+//        int tDay = Integer.parseInt(date1[1]);
+//        int tYear = Integer.parseInt(date1[2]);
+//        int tHour = Integer.parseInt(date1[0]);
+//        int tMinute = Integer.parseInt(date1[1]);
+//
+//        int aMonth = Integer.parseInt(date2[0]);
+//        int aDay = Integer.parseInt(date2[1]);
+//        int aYear = Integer.parseInt(date2[2]);
+//        int aHour = Integer.parseInt(date2[0]);
+//        int aMinute = Integer.parseInt(date2[1]);
+//
+//        int cMonth = Math.abs(aMonth - tMonth);
+//        int cDay = Math.abs(aDay - tDay);
+//        int cYear = Math.abs(aYear - tYear);
+//        int cHour = Math.abs(aHour - tHour);
+//        int cMinute = Math.abs(aMinute - tMinute);
+//
+//        if(cMonth == 0 && cYear == 0){
+//            int total = cMinute*60*1000 + cHour*3600*1000 + cDay*24*3600*1000;
+//            return total;
+//        }
+//        int total = 9999999;
+//
+//        return total;
+//    }
+
+    public static int[] getReminderTime(Reminder reminder){
+        String[] date2 = reminder.getDate().split("/");
+        String[] time2 = reminder.getTime().split(":");
+        int aMonth = Integer.parseInt(date2[0]);
+        int aDay = Integer.parseInt(date2[1]);
+        int aYear = Integer.parseInt(date2[2]);
+        int hour = Integer.parseInt(time2[0]);
+        int minute = Integer.parseInt(time2[1]);
+        int[] times = new int[]{aMonth, aDay, aYear, hour, minute};
+        return times;
     }
 
 }
