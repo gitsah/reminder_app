b0b86043b91af98c5c456ea183b5838770571960
Sahand-Admin <ghotbijr@gmail.com>
2018-06-10 22:55:13 -0700

    Cleaned up

--- app/src/main/java/com/ad340/group3/reminder_app/AlarmReceiver.java
+++ app/src/main/java/com/ad340/group3/reminder_app/AlarmReceiver.java
@@ -23,28 +23,17 @@ public class AlarmReceiver extends BroadcastReceiver{
     }
 
     public static void setAlarm(Context context, Reminder reminder){
-        // get a Calendar object with current time
-        //Calendar calender = Calendar.getInstance();
-        // add 30 seconds to the calendar object
         int[] times = SecondActivity.getReminderTime(reminder);
         Calendar calendar = new GregorianCalendar(times[2], times[0], times[1], times[3], times[4]);
-//        calender.set(Calendar.HOUR_OF_DAY, times[3]);
-//        calender.set(Calendar.MINUTE, times[4]);
-//        calender.set(Calendar.YEAR, times[2]);
-//        calender.set(Calendar.MONTH, times[0]);
-//        calender.set(Calendar.DAY_OF_MONTH, times[1]);
 
         Intent intent = new Intent(context, AlarmReceiver.class);
         PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, 0);
         // Get the AlarmManager service
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        System.out.println("system time: " + System.currentTimeMillis() + " our time: " + calendar.getTimeInMillis());
         if (am != null) {
             am.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), sender);
         }
         System.out.println(calendar);
-
-        Log.d("Carbon","Alrm SET !!");
     }
 
 //    public static void cancelAlarm(AlarmManager am){
